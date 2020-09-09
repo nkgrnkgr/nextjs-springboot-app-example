dc862afc93dda7bc5864f65b7ad580f0e1ae81f7
nkgrnkgr <beefupme@gmail.com>
2020-09-09 17:46:09 +0900

    init

---
+++ backend-server/src/main/kotlin/com/ngkrnkgr/backendserver/api/UserController.kt
@@ -0,0 +1,18 @@
+package com.ngkrnkgr.backendserver.api
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/user")
+class EyeCatchController(
+) {
+
+    @GetMapping
+    fun getHoge(): String {
+        return "hoge"
+    }
+}
---
+++ backend-server/src/test/kotlin/com/ngkrnkgr/backendserver/BackendServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ngkrnkgr.backendserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
